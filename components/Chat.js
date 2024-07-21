bc464f64acd9ad8bf69e579c12d28a70719eec17
alexmathews89 <alexmathews@live.com>
2024-07-21 16:45:14 -0700

    added Gifted Chat to chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,66 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, color } = route.params;
+  const [messages, setMessages] = useState([]);
 
+  //sets the user's name as the title
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
 
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: "You have entered the chat",
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
+  // Appends a new message to an array of previous messages
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
   return (
     <View style={[styles.container, { backgroundColor: color }]}>
-      <Text>Hello Screen2!</Text>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+
+      {Platform.OS === "ios" ? (
+        <KeyboardAvoidingView behavior="padding" />
+      ) : null}
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
     </View>
   );
 };
@@ -18,9 +68,11 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    //backgroundColor: color,
+    //justifyContent: "center",
+    //alignItems: "center",
+  },
+  textColor: {
+    color: "#b00b1e",
   },
 });
 
